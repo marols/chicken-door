66453e2de0b05f6d00fbdd78ff11f909b1575c20
Martin Walter <martin.walter.swe@gmail.com>
2019-09-12 10:15:18 +0200

    Adding possibility to offset open and close times

--- ChickenDoor.ino
+++ ChickenDoor.ino
@@ -10,39 +10,19 @@
 #include <Ethernet2.h>
 #include <BlynkSimpleEthernet2.h>
 
-// Blynk auth token
+// Variables accessed by Blink
 char auth[] = "EFirYfEkmK7Kud24TrEptNQ1Gj8g-msn";
 int blynkUpButtonPressed = LOW;
 int blynkDownButtonPressed = LOW;
-
-// Virtual Blynk UP button handler
-BLYNK_WRITE(V0)
-{
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-
-  if(pinValue) {
-    Serial.print("UP: ");
-    Serial.println(pinValue);
-    blynkUpButtonPressed = HIGH;
-  }
-}
-
-// Virtual Blynk DOWN button handler
-BLYNK_WRITE(V1)
-{
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  if(pinValue) {
-    Serial.print("DOWN: ");
-    Serial.println(pinValue);
-    blynkDownButtonPressed = HIGH;
-  }
-}
+byte closeOffset = 0;
+byte openOffset = 0;
 
 // Set open and close times
-byte startHour=7;
-byte startMinute=00;
-byte endHour= 20;
-byte endMinute= 30;
+byte defaultStartHour = 7;
+byte startHour = 7;
+byte startMinute = 00;
+byte endHour = 19;
+byte endMinute = 00;
 
 tmElements_t tm, tm_hour_start, tm_hour_end;
 time_t now_, t_hour_start, t_hour_end;
@@ -55,7 +35,8 @@ bool manualOverride = 0;
 int timeoutCounter = 3000;
 bool timerOn = 0;
 
-int ledPin = 10; // choose the pin for the LED
+// Choose pin for the LED
+int ledPin = 35;
 
 // Initialize buttons
 int inButtonUp = 28;   // choose the input pin (for a pushbutton)
@@ -69,15 +50,6 @@ int inTouchDown = 24;   // choose the input pin (for a pushbutton)
 int valTouchUp = 0;     // variable for reading the pin status
 int valTouchDown = 0;     // variable for reading the pin statu
 
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-
-// Create an instance of the TFT library
-LCDShield lcd;  // Creates an LCDShield, named lcd
-
 // Initialize status messages
 char doorPrintout[6];
 char enginePrintout[6];
@@ -85,21 +57,80 @@ char systemPrintout[6];
 char openTimePrintout[6];
 char closeTimePrintout[6];
 
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+
+// Create an instance of the TFT library
+LCDShield lcd;  // Creates an LCDShield, named lcd
 
 // TimeLord settings
 float const LONGITUDE = 13.00073;
 float const LATITUDE = 55.60587;
-TimeLord tardis; 
+TimeLord tardis;
 byte today[] = { 0, 0, 12, 9, 7, 19 }; // store today's date (at noon) in an array for TimeLord to use
 
 // 24 hour loop variable
 time_t previousWeekday;
 
+// Synk with Blynk server
+BLYNK_CONNECTED() {
+  Blynk.syncVirtual(V2);
+  Blynk.syncVirtual(V3);
+}
+
+// Virtual Blynk UP button handler
+BLYNK_WRITE(V0)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+  if (pinValue) {
+    Serial.print("UP: ");
+    Serial.println(pinValue);
+    blynkUpButtonPressed = HIGH;
+  }
+}
+
+// Virtual Blynk DOWN button handler
+BLYNK_WRITE(V1)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  if (pinValue) {
+    Serial.print("DOWN: ");
+    Serial.println(pinValue);
+    Serial.println();
+    blynkDownButtonPressed = HIGH;
+  }
+}
+
+// Virtual Blynk OPEN OFFSET slider handler
+BLYNK_WRITE(V2)
+{
+  int stepperValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  Serial.print("New open offset: ");
+  Serial.println(stepperValue);
+  openOffset = stepperValue;
+  updateOpenTime();
+}
+
+// Virtual Blynk CLOSE OFFSET slider handler
+BLYNK_WRITE(V3)
+{
+  int stepperValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  Serial.print("New close offset: ");
+  Serial.println(stepperValue);
+  closeOffset = stepperValue;
+  updateClosingTime();
+}
+
+
 void setup() {
   Serial.begin(9600);           // Set up Serial library at 9600 bps
   while (!Serial) ; // Wait for serial
   delay(200);
-  
+
   Serial.println("Chickenz running!");
   Serial.println("-------------------");
 
@@ -123,7 +154,7 @@ void setup() {
   lcd.init(PHILIPS);  // Initializes lcd, using an PHILIPSdriver
   lcd.contrast(-61);  // -51's usually a good contrast value
   lcd.clear(WHITE);  // clear the screen
-  
+
   // Print static text
   lcd.setStr("< Chickenz >", 2, 15, BLACK, WHITE);
   lcd.setLine(23, 5, 23, 125, BLACK);
@@ -133,37 +164,44 @@ void setup() {
   lcd.setStr("Open:", 85, 5, BLACK, WHITE);
   lcd.setStr("Close:", 105, 5, BLACK, WHITE);
 
+  // Dabble with time
+  updateTimeStructures();
+
+  // TimeLord setup
+  tardis.TimeZone(2 * 60);
+  tardis.Position(LATITUDE, LONGITUDE); // tell TimeLord where in the world we are
+  tardis.DstRules(3, 4, 10, 4, 60); // Sista söndagen i mars till sista söndagen i oktober
+
+  // Init 24 hour loop
+  previousWeekday = now();
+
+  // Update times
+  updateOpenTime();
+  updateClosingTime();
+
   // Print default status messages
   printStatus(doorPrintout, "Shut", 25, V5);
   printStatus(enginePrintout, "Off", 45, V6);
   printStatus(systemPrintout, "Auto", 65, V7);
   printStatus(openTimePrintout, getOpenTime(), 85, V8);
   printStatus(closeTimePrintout, getCloseTime(), 105, V9);
-
-  // TimeLord setup
-  tardis.TimeZone(2*60);
-  tardis.Position(LATITUDE, LONGITUDE); // tell TimeLord where in the world we are
-  tardis.DstRules(3,4,10,4, 60); // Sista söndagen i mars till sista söndagen i oktober
-
-  // Init 24 hour loop
-  previousWeekday = now();
 }
 
 void loop() {
   // Blynk start
   Blynk.run();
-  
-  // Read input value from physical and virtual Blynk buttons 
+
+  // Read input value from physical and virtual Blynk buttons
   valButtonUp = !digitalRead(inButtonUp) || blynkUpButtonPressed;
   valButtonDown = !digitalRead(inButtonDown) || blynkDownButtonPressed;
 
-  // Read door sensors  
+  // Read door sensors
   valTouchUp = digitalRead(inTouchUp);
-  valTouchDown = digitalRead(inTouchDown); 
+  valTouchDown = digitalRead(inTouchDown);
 
   // Tick safety timeout counter
-  if(timerOn && timeoutCounter>=0) {
-    timeoutCounter = timeoutCounter-1;
+  if (timerOn && timeoutCounter >= 0) {
+    timeoutCounter = timeoutCounter - 1;
     Serial.println(timeoutCounter);
   }
 
@@ -172,60 +210,35 @@ void loop() {
     digitalWrite(ledPin, HIGH);
     Serial.print("Touching up\n");
     doorIsUp();
+    digitalWrite(ledPin, LOW);
   } else if (valTouchDown == HIGH && doorIsOpen && engineRunning) {
     digitalWrite(ledPin, HIGH);
     Serial.print("Touching down\n");
     doorIsDown();
+    digitalWrite(ledPin, LOW);
   } else if (timeoutCounter == 0) {
+    digitalWrite(ledPin, HIGH);
     Serial.println("Safety timeout reached, door stopped");
     doorFailed();
     printStatus(systemPrintout, "Fail", 65, V7);
-  } else {
     digitalWrite(ledPin, LOW);
   }
-  
-  // Get current timestamp
-  now_ = RTC.get();
-  
-  // Make current date and time structure
-  breakTime(now_, tm);
-  
-  // Make auxiliary structures to be more human editable and friendly
-  if(update_tm){
-    memcpy(&tm_hour_start, &tm, sizeof(tm));
-    memcpy(&tm_hour_end, &tm, sizeof(tm));
-    
-    // Change auxiliary structures to meet your start and end schedule 
-    tm_hour_start.Hour = startHour;
-    tm_hour_start.Minute = startMinute;
-    tm_hour_start.Second = 0;
-    tm_hour_end.Hour = endHour;
-    tm_hour_end.Minute = endMinute;
-    tm_hour_end.Second = 0;
-    
-    // Reverse process to get timestamps
-    t_hour_start = makeTime(tm_hour_start);
-    t_hour_end = makeTime(tm_hour_end);
 
-    // Check if end time is past midnight and correct if needed
-    if (startHour > endHour) //past midnight correction
-      t_hour_end = t_hour_end + SECS_PER_DAY;
-  }
-  //final part   
+  updateTimeStructures();
 
   if (!manualOverride) {
-    if ((t_hour_start <= now_) && (now_ <= t_hour_end)){
-      /* if we got a valid schedule, don't change the tm_hour structures and the 
-      respective t_hour_start and t_hour_end timestamps. They should be updated 
-      after exiting the valid schedule */
-      if(update_tm)  
+    if ((t_hour_start <= now_) && (now_ <= t_hour_end)) {
+      /* if we got a valid schedule, don't change the tm_hour structures and the
+        respective t_hour_start and t_hour_end timestamps. They should be updated
+        after exiting the valid schedule */
+      if (update_tm)
         update_tm = 0;
-  
+
       // Do something if current time is inside interval
       openDoor();
-      
-    }else{
-      if(update_tm == 0)  
+
+    } else {
+      if (update_tm == 0)
         update_tm = 1;
       // Do something else if current time is outside interval
       closeDoor();
@@ -238,7 +251,7 @@ void loop() {
     Serial.print("Re-activating automatic schedule");
     manualOverride = false;
     printStatus(systemPrintout, "Auto", 65, V7);
-    delay(1000);
+    digitalWrite(ledPin, LOW);
   } else {
     if (valButtonUp == HIGH) {         // check if the input is HIGH (button released)
       digitalWrite(ledPin, HIGH);  // turn LED ON
@@ -246,38 +259,22 @@ void loop() {
       manualOverride = true;
       printStatus(systemPrintout, "Man", 65, V7);
       openDoor();
+      digitalWrite(ledPin, LOW);
     } else if (valButtonDown == HIGH) {         // check if the input is HIGH (button released)
       digitalWrite(ledPin, HIGH);  // turn LED ON
       Serial.print("Manually closing door\n");
       manualOverride = true;
       printStatus(systemPrintout, "Man", 65, V7);
       closeDoor();
-    } else {
-      digitalWrite(ledPin, LOW);  // turn LED OFF
+      digitalWrite(ledPin, LOW);
     }
   }
 
   // Daily check update of sunset time
   time_t currentWeekday = weekday(now());
-  
-  if (currentWeekday != previousWeekday) { // If it is a new day, update sunset time
-    // Update today's date
-    today[3] = tm.Day;
-    today[4] = tm.Month;
-    today[5] = tm.Year; 
 
-    // Check sunset time
-    if (tardis.SunSet(today)) { // if the sun will set today (it might not, in the [ant]arctic)    
-       Serial.print("Sunset: ");
-       Serial.print((int) today[tl_hour]);
-       Serial.print(":");
-       Serial.println((int) today[tl_minute]);
-       endHour = today[tl_hour] - 1;
-       endMinute = today[tl_minute];
-       printStatus(closeTimePrintout, getCloseTime(), 105, V9);
-       previousWeekday = currentWeekday;
-     }
-     Serial.println();
+  if (currentWeekday != previousWeekday) { // If it is a new day, update sunset time
+    updateClosingTime();
   }
 
   // Reset Blynk button values
@@ -285,14 +282,70 @@ void loop() {
   blynkDownButtonPressed = LOW;
 }
 
+void updateTimeStructures() {
+  // Get current timestamp
+  now_ = RTC.get();
+
+  // Make current date and time structure
+  breakTime(now_, tm);
+
+  // Make auxiliary structures to be more human editable and friendly
+  if (update_tm) {
+    memcpy(&tm_hour_start, &tm, sizeof(tm));
+    memcpy(&tm_hour_end, &tm, sizeof(tm));
+
+    // Change auxiliary structures to meet your start and end schedule
+    tm_hour_start.Hour = startHour;
+    tm_hour_start.Minute = startMinute;
+    tm_hour_start.Second = 0;
+    tm_hour_end.Hour = endHour;
+    tm_hour_end.Minute = endMinute;
+    tm_hour_end.Second = 0;
+
+    // Reverse process to get timestamps
+    t_hour_start = makeTime(tm_hour_start);
+    t_hour_end = makeTime(tm_hour_end);
+
+    // Check if end time is past midnight and correct if needed
+    if (startHour > endHour) //past midnight correction
+      t_hour_end = t_hour_end + SECS_PER_DAY;
+  }
+}
+
+void updateOpenTime() {
+  startHour = defaultStartHour + openOffset;
+  printStatus(openTimePrintout, getOpenTime(), 85, V8);
+}
+
+void updateClosingTime() {
+  // Update today's date
+  today[3] = tm.Day;
+  today[4] = tm.Month;
+  today[5] = tm.Year;
+
+  // Check sunset time
+  if (tardis.SunSet(today)) { // if the sun will set today (it might not, in the [ant]arctic)
+
+    Serial.print("Sunset: ");
+    Serial.print(print2digits((int) today[tl_hour]));
+    Serial.print(":");
+    Serial.println(print2digits((int) today[tl_minute]));
+    endHour = today[tl_hour] + closeOffset;
+    endMinute = today[tl_minute];
+
+    previousWeekday = weekday(now());
+  }
+  printStatus(closeTimePrintout, getCloseTime(), 105, V9);
+}
+
 void doorMove(uint8_t direction) {
   uint8_t i;
-  
+
   printStatus(enginePrintout, "On", 45, V6);
-  
+
   myMotor->run(direction);
-  for (i=0; i<255; i++) {
-    myMotor->setSpeed(i);  
+  for (i = 0; i < 255; i++) {
+    myMotor->setSpeed(i);
     delay(5);
   }
   engineRunning = 1;
@@ -307,28 +360,28 @@ void stopDoor() {
 }
 
 void doorIsDown() {
-    stopDoor();
-    doorIsOpen = 0;
-    Serial.print("Door closed\n");
-    printStatus(doorPrintout, "Shut", 25, V5);
+  stopDoor();
+  doorIsOpen = 0;
+  Serial.print("Door closed\n");
+  printStatus(doorPrintout, "Shut", 25, V5);
 }
 
 void doorIsUp() {
-    stopDoor();
-    doorIsOpen = 1;
-    Serial.print("Door open\n");
-    printStatus(doorPrintout, "Open", 25, V5);
+  stopDoor();
+  doorIsOpen = 1;
+  Serial.print("Door open\n");
+  printStatus(doorPrintout, "Open", 25, V5);
 }
 
 void doorFailed() {
-    stopDoor();
-    doorIsOpen = 1;
-    Serial.print("Door failed. Press down to reset\n");
-    printStatus(doorPrintout, "?", 25, V5);
+  stopDoor();
+  doorIsOpen = 1;
+  Serial.print("Door failed. Press down to reset\n");
+  printStatus(doorPrintout, "?", 25, V5);
 }
 
-void closeDoor() {  
-  if(doorIsOpen && !engineRunning) {
+void closeDoor() {
+  if (doorIsOpen && !engineRunning) {
     printStatus(doorPrintout, "\\/", 25, V5);
     startTimer(1800);
     doorMove(FORWARD);
@@ -336,7 +389,7 @@ void closeDoor() {
 }
 
 void openDoor() {
-  if(!doorIsOpen && !engineRunning) {
+  if (!doorIsOpen && !engineRunning) {
     printStatus(doorPrintout, "/\\", 25, V5);
     startTimer(2000);
     doorMove(BACKWARD);
@@ -358,6 +411,7 @@ String getOpenTime() {
   String openTime = print2digits(startHour) + ":" + print2digits(startMinute);
   Serial.print("Open time is: ");
   Serial.println(openTime);
+  Serial.println();
   return openTime;
 }
 
@@ -365,6 +419,7 @@ String getCloseTime() {
   String closeTime = print2digits(endHour) + ":" + print2digits(endMinute);
   Serial.print("Close time is: ");
   Serial.println(closeTime);
+  Serial.println();
   return closeTime;
 }
 
